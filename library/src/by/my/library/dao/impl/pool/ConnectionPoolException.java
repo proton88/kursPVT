cb4ffc530937a59d838d8e65b63d9c4942e2c0ea
proton88 <designskinali@gmail.com>
2016-08-29 11:34:02 +0200

    fix my error in db.properties

--- library/src/by/my/library/dao/impl/pool/ConnectionPoolException.java
+++ library/src/by/my/library/dao/impl/pool/ConnectionPoolException.java
@@ -1,6 +1,8 @@
 package by.my.library.dao.impl.pool;
 
 public class ConnectionPoolException extends Exception{
+	private static final long serialVersionUID = 1L;
+
 	public ConnectionPoolException(String message){
 		super(message);
 	}
